cc0a6e97b2d4b2a9c38646200c5284411f069a65
shireesh <shireesh.gadidesi@cloudera.com>
2023-06-13 02:37:44 +0530

    update prefix.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
+
 const nextConfig = {
   output: 'export',
   distDir: 'docs',
+  assetPrefix: isProd ? 'https://gshireesh.github.io/dynamicform/' : undefined,
 }
 
 module.exports = nextConfig
