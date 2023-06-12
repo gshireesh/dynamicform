19fdf51417ec6ff34e746e0c73e3d9f952cc45a3
shireesh <shireesh.gadidesi@cloudera.com>
2023-06-13 02:52:14 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   distDir: 'docs',
+  basePath: '/dynamicform',
   assetPrefix: isProd ? 'https://gshireesh.github.io/dynamicform/' : undefined,
 }
 
